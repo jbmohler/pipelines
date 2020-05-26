ef5889fea01f85a60f9046b5e9ca44c0d1ec2e2e
Joel Mohler <jmohler@conducto.com>
2020-05-26 09:31:15 -0400

    pipeline/user data example and lazy example

--- docker_build_arg.py
+++ docker_build_arg.py
@@ -9,8 +9,8 @@ RUN echo $my_arg > /content.txt""")
 
 def argged() -> co.Serial:
     output = co.Serial()
-    output['node1'] = co.Exec("cat /content.txt", image=co.Image(dockerfile="Dockerfile.buildarg", dockerfile_build_args={"my_arg": "node1"}))
-    output['node2'] = co.Exec("cat /content.txt", image=co.Image(dockerfile="Dockerfile.buildarg", dockerfile_build_args={"my_arg": "node2"}))
+    output['node1'] = co.Exec("cat /content.txt", image=co.Image(dockerfile="Dockerfile.buildarg", docker_build_args={"my_arg": "node1"}))
+    output['node2'] = co.Exec("cat /content.txt", image=co.Image(dockerfile="Dockerfile.buildarg", docker_build_args={"my_arg": "node2"}))
     return output
 
 if __name__ == '__main__':
--- tempdatatest.py
+++ tempdatatest.py
@@ -1,17 +1,29 @@
 import conducto as co
 
-def save_temp_data(index):
-    co.temp_data.puts(name="data_chunk", obj=b'asdf '+bytes(str(index), 'utf-8'))
+def save_user_data(index):
+    co.data.pipeline.puts(name="data_chunk", obj=b'asdf '+bytes(str(index), 'utf-8'))
 
-def save_perm_data(index):
-    co.perm_data.puts(name="data_chunk", obj=b'asdf '+bytes(str(index), 'utf-8'))
+def save_pipeline_data(index):
+    co.data.user.puts(name="data_chunk", obj=b'asdf '+bytes(str(index), 'utf-8'))
+
+def read_user_data():
+    data = co.data.pipeline.gets(name="data_chunk")
+    print(data)
+
+def read_pipeline_data():
+    data = co.data.user.gets(name="data_chunk")
+    print(data)
 
 def run() -> co.Serial:
-    output = co.Serial(image=co.Image(copy_dir='.'))
-    output["save_temp_data1"] = co.Exec(save_temp_data, 1) 
-    output["save_perm_data1"] = co.Exec(save_perm_data, 1) 
-    output["save_temp_data2"] = co.Exec(save_temp_data, 2) 
-    output["save_perm_data2"] = co.Exec(save_perm_data, 2) 
+    co.Image.register_directory("JOEL_PIPELINES", ".")
+
+    output = co.Serial(image=co.Image(copy_dir='${JOEL_PIPELINES}'))
+    output["save_user_data1"] = co.Exec(save_user_data, 1)
+    output["save_pipeline_data1"] = co.Exec(save_pipeline_data, 1)
+    output["save_user_data2"] = co.Exec(save_user_data, 2)
+    output["save_pipeline_data2"] = co.Exec(save_pipeline_data, 2)
+    output["read_user_data2"] = co.Exec(read_user_data)
+    output["read_pipeline_data2"] = co.Exec(read_pipeline_data)
 
     return output
 
