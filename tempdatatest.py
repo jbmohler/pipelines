dd717562a5281e20c2998f3cb754d024525ae682
Joel B. Mohler <jmohler@conducto.com>
2020-07-07 16:16:24 +0000

    reqdocker and data test improvements

--- tempdatatest.py
+++ tempdatatest.py
@@ -1,17 +1,25 @@
 import conducto as co
 
-def save_user_data(index):
-    co.data.pipeline.puts(name="data_chunk", obj=b'asdf '+bytes(str(index), 'utf-8'))
+def save_user_data(index: int):
+    co.data.pipeline.puts(name="data/chunk", obj=b'asdf '+bytes(str(index), 'utf-8'))
+    if index == 2:
+        co.data.pipeline.puts(name="data/chunk2", obj=b'asdf '+bytes(str(index), 'utf-8'))
 
-def save_pipeline_data(index):
-    co.data.user.puts(name="data_chunk", obj=b'asdf '+bytes(str(index), 'utf-8'))
+def save_pipeline_data(index: int):
+    co.data.user.puts(name="data/chunk", obj=b'asdf '+bytes(str(index), 'utf-8'))
+    if index == 2:
+        co.data.user.puts(name="data/chunk2", obj=b'asdf '+bytes(str(index), 'utf-8'))
 
 def read_user_data():
-    data = co.data.pipeline.gets(name="data_chunk")
+    print("data list")
+    print(co.data.user.list("data"))
+    data = co.data.pipeline.gets(name="data/chunk")
     print(data)
 
 def read_pipeline_data():
-    data = co.data.user.gets(name="data_chunk")
+    print("data list")
+    print(co.data.pipeline.list("data"))
+    data = co.data.user.gets(name="data/chunk")
     print(data)
 
 def run() -> co.Serial:
@@ -22,8 +30,8 @@ def run() -> co.Serial:
     output["save_pipeline_data1"] = co.Exec(save_pipeline_data, 1)
     output["save_user_data2"] = co.Exec(save_user_data, 2)
     output["save_pipeline_data2"] = co.Exec(save_pipeline_data, 2)
-    output["read_user_data2"] = co.Exec(read_user_data)
-    output["read_pipeline_data2"] = co.Exec(read_pipeline_data)
+    output["read_user_data"] = co.Exec(read_user_data)
+    output["read_pipeline_data"] = co.Exec(read_pipeline_data)
 
     return output
 
