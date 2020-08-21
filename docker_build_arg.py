3e12d5eae057c2a40e520d542d20bec655d2ac2f
Joel Mohler <jmohler@conducto.com>
2020-08-21 13:42:18 -0400

    better buildarg

--- docker_build_arg.py
+++ docker_build_arg.py
@@ -1,10 +1,15 @@
+import uuid
 import conducto as co
 
 
 def argged() -> co.Serial:
     output = co.Serial()
-    output['node1'] = co.Exec("cat /content.txt", image=co.Image(dockerfile="Dockerfile.buildarg", docker_build_args={"my_arg": "node1"}))
-    output['node2'] = co.Exec("cat /content.txt", image=co.Image(dockerfile="Dockerfile.buildarg", docker_build_args={"my_arg": "node2"}))
+    output['node1'] = co.Exec("cat /content.txt", image=co.Image(dockerfile="Dockerfile.buildarg", docker_build_args={"my_arg": f"node1 - {uuid.uuid1().hex}"}))
+    output['node2'] = co.Exec("cat /content.txt", image=co.Image(dockerfile="Dockerfile.buildarg", docker_build_args={"my_arg": f"node2 - {uuid.uuid1().hex}"}))
+    output['node3'] = co.Exec("cat /content.txt", image=co.Image(dockerfile="Dockerfile.buildarg", docker_build_args={"my_arg": f"node3 - {uuid.uuid1().hex}"}))
+    output['node4'] = co.Exec("cat /content.txt", image=co.Image(dockerfile="Dockerfile.buildarg", docker_build_args={"my_arg": f"node4 - {uuid.uuid1().hex}"}))
+    output['node5'] = co.Exec("cat /content.txt", image=co.Image(dockerfile="Dockerfile.buildarg", docker_build_args={"my_arg": f"node5 - {uuid.uuid1().hex}"}))
+    output['node6'] = co.Exec("cat /content.txt", image=co.Image(dockerfile="Dockerfile.buildarg", docker_build_args={"my_arg": f"node6 - {uuid.uuid1().hex}"}))
     return output
 
 if __name__ == '__main__':
